2777b480edce7859ee9dad9675727779e28bcebc
Dhruv Thakkar <dhruvthakkar1768@gmail.com>
2022-10-26 20:24:28 -0400

    nav-bar update

--- backend/controllers/news.js
+++ backend/controllers/news.js
@@ -39,7 +39,10 @@ exports.newsSearch = async(req,res)=>{
         var url = `http://newsapi.org/v2/everything?q=${search}&${`apiKey=${process.env.NEWSAPIKEY}`}`
 
         const news_get =await axios.get(url)
-        res.render('news',{articles:news_get.data.articles})
+
+        return res.json(news_get)
+
+        //res.render('news',{articles:news_get.data.articles})
     } catch (error) {
         if(error.response){
             console.log(error)
