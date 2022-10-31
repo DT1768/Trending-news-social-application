4007407c4151327aeea6bc6e81bf0fda891f2e2b
Dhruv Thakkar <dhruvthakkar1768@gmail.com>
2022-10-31 16:51:19 -0400

    trendsapi and react

--- v1/app.js
+++ v1/app.js
@@ -7,13 +7,15 @@ const bodyParser = require('body-parser');
 const moment = require('moment')
 app.locals.moment = moment;
 
+const newsRoutes = require('./routes/news');
+
 // template engine  
-app.use(express.static('views'))
-app.set('view engine','ejs')
+//app.use(express.static('views'))
+//app.set('view engine','ejs')
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/',require('./routes/news'))
+app.use(bodyParser.json());
+app.use('/',newsRoutes)
 
-app.set('views','./views')
 
 app.listen(port,()=> console.log("started"))
