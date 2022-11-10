c2a4c39078ad9765894e1a527ebcf934d966ba08
tejas-dobariya <a9510262243@gmail.com>
2022-11-10 02:03:58 -0500

    main card refactoring

--- v1/Frontend/news/src/App.js
+++ v1/Frontend/news/src/App.js
@@ -8,7 +8,7 @@ import LoadingBar from "react-top-loading-bar";
 
 const App = () => {
   const pageSize = 5;
-  const apiKey = process.env.REACT_APP_NEWS_API;
+  const apiKey = "228354b32a7b422eaa8cdd5c521cbc8d";
   const [progress, setProgress] = useState(0);
 
   return (
