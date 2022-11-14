d7f368b8fcd6bcc7f002f04ceb3b9aecf2144f17
Dhruv Thakkar <dhruvthakkar1768@gmail.com>
2022-11-14 15:54:24 -0500

    oauth backend final

---
+++ backend/Routes/user.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+//const { google, googleCallback } = require("../controllers/user");
+const passport = require("passport");
+const { myProfile,logOut, isAuthenticated, updatePrefrences } = require("../controllers/user");
+const router = express.Router();
+
+
+router.get('/googlelogin',passport.authenticate("google",{
+    scope: ["profile","email"],
+}));
+
+/*router.get('/login',passport.authenticate("google",{
+    scope: ["profile","email"],
+    
+    //successRedirect: process.env.FRONTENDURL,
+}));*/
+
+router.get('/login',passport.authenticate("google"),(req,res,next) => {
+    if(req.user.preferences == undefined || req.user.location == undefined){
+        //res.json(req.user);
+        //successRedirect: process.env.FRONTENDURL,
+        //call update prefrences.
+        res.send("Logged In.");
+    }
+    else{
+        //successRedirect: process.env.FRONTENDURL,
+        res.send("Logged In.");
+    }
+});
+
+router.put('/updateprofile', isAuthenticated, updatePrefrences);
+
+router.get('/me',isAuthenticated , myProfile);
+
+router.get('/logout',logOut);
+module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,56 @@
+
+//Library Imports
 require('dotenv').config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-
+const session = require("express-session");
 const app = express();
 
 const bodyParser = require("body-parser");
-const googleTrends = require("google-trends-api");
-//const cookieParser = require("cookie-parser");
-//const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+//Routes Imports
 const newsRoutes = require("./routes/news");
 const categoryRoutes = require("./routes/category");
 const subcategoryRoutes = require("./routes/subcategory");
 const locationRoutes = require("./routes/location");
+const userRoutes = require("./routes/user");
+const {connectPassport} = require("./config/passport");
+const passport = require('passport');
 
+//DB Connect
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true,
     useUnifiedTopology: true 
-})
-    .then( () => {
+}).then( () => {
         console.log('Connected to the database ')
-    })
-    .catch( (err) => {
+    }).catch( (err) => {
         console.error(`Error connecting to the database. n${err}`);
     })
 
+//using Middlewares
+app.use(session({
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(cookieParser());
-//app.use(cors());
+app.use(cookieParser());
+app.use(cors());
 
+
+connectPassport();
 app.use("/api", newsRoutes);
 app.use("/api",categoryRoutes);
 app.use("/api",subcategoryRoutes);
 app.use("/api",locationRoutes);
+app.use("/api",userRoutes);
 
 const port = process.env.PORT || 8000;
 
