7504c257bcf8416878b96c2d24a18c2914b996aa
Dhruvkumar Thakkar <dhruvthakkar1768@gmail.com>
2022-12-02 14:33:14 -0500

    Update app.js

--- v3/backend/app.js
+++ v3/backend/app.js
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://social-media-news-application.herokuapp.com',
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }));
